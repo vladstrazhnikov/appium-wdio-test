b9232e54c7eb2cb1d9d5387c2cf6fdb5859b2ffb
vladstrazhnikov <vladislav.strazhnikov@gmail.com>
2023-02-07 17:14:08 +0400

    fixes

--- test/pageobjects/saved.page.ts
+++ test/pageobjects/saved.page.ts
@@ -1,5 +1,4 @@
 export class SavedPage {
-    // private savedArticles = $('android.widget.FrameLayout/android.view.ViewGroup/android.view.ViewGroup/android.widget.LinearLayout/androidx.recyclerview.widget.RecyclerView/android.view.ViewGroup');
     get savedArticle() {
         return $('//android.widget.TextView[@resource-id="org.wikipedia:id/page_list_item_title"]');
     }
@@ -8,9 +7,4 @@ export class SavedPage {
         const savedArticles = await $('//android.widget.FrameLayout/android.view.ViewGroup/android.view.ViewGroup/android.widget.LinearLayout/androidx.recyclerview.widget.RecyclerView/android.view.ViewGroup');
         savedArticles.click();
     };
-
-    // async openSavedArticles(){
-    //     await this.savedArticles.click();
-    //     return this;
-    // };
 }
